1dff654b054ac8eaccc5f9e759f1bbc2b4ffd07f
Jaddu5522 <65483209+Jaddu5522@users.noreply.github.com>
2025-01-25 14:25:35 +0530

    Update Singleton.kt - Added HttpLoggingInterceptor

--- Singleton.kt
+++ Singleton.kt
@@ -5,8 +5,13 @@ object RetrofitInstance{
   private const val BASE_URL="http://example.com/"
 
   fun getInstance(): Retrofit {
+    val client = OkHttpClient()
+    val interceptor = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+    val clientBuilder = OkHttpClient.Builder = client.newBuilder().addInterceptor(interceptor as HttpLoggingInterceptor)
+    
     return Retrofit.Builder().baseUrl(BASE_URL)
       .addConverterFactory(GsonConverterFactory.create())
+      .client(clientBuilder.build())
       .build()
   }
 
