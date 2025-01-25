bf44da837fa0d6e95d225b9f8b33974a823a5ea8
Jaddu5522 <65483209+Jaddu5522@users.noreply.github.com>
2025-01-25 14:03:23 +0530

    Create Singleton.kt - Added Retrofit - Singleton

---
+++ Singleton.kt
@@ -0,0 +1,14 @@
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitInstance{
+  private const val BASE_URL="http://example.com/"
+
+  fun getInstance(): Retrofit {
+    return Retrofit.Builder().baseUrl(BASE_URL)
+      .addConverterFactory(GsonConverterFactory.create())
+      .build()
+  }
+
+  
+}
